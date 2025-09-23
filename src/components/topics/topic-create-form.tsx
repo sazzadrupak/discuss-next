7e9a8f6135535643c2baebad5d515d378976e094
sazzadrupak <sazzad@oployeelabs.com>
2025-09-23 23:49:41 +0300

    Handling general form errors

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -52,6 +52,11 @@ export default function TopicCreateForm() {
               isInvalid={!!formState.errors.description}
               errorMessage={formState.errors.description?.join(', ')}
             />
+            {formState.errors._form && (
+              <div className="p-2 bg-red-200 border border-red-600 rounded">
+                {formState.errors._form?.join(', ')}
+              </div>
+            )}
             <Button type="submit">Submit</Button>
           </div>
         </form>
