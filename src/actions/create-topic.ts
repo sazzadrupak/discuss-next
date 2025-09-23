ff6c6f5e63e82cca6d265c9048ba886dafd8a4e5
sazzadrupak <sazzad@oployeelabs.com>
2025-09-23 23:43:34 +0300

    Display validation error

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,37 @@
 'use server';
 
-export async function createTopic() {
+import { z } from 'zod';
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: 'Must be lowercase letters or dashes without spaces',
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const name = formData.get('name')?.toString();
+  const description = formData.get('description')?.toString();
+  const result = createTopicSchema.safeParse({ name, description });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+  return { errors: {} };
   // TODO: revalidate the homepage
 }
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Button,
   Input,
@@ -6,30 +8,49 @@ import {
   PopoverTrigger,
   Textarea,
 } from '@nextui-org/react';
+import { startTransition, useActionState } from 'react';
 
 import * as actions from '@/actions';
 
 export default function TopicCreateForm() {
+  const [formState, action] = useActionState(actions.createTopic, {
+    errors: {},
+  });
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    startTransition(() => {
+      action(formData);
+    });
+  }
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form onSubmit={handleSubmit} noValidate>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg font-medium">Create New Topic</h3>
             <Input
+              name="name"
               label="Name"
               labelPlacement="outside"
-              placeholder="Enter topic title"
-              aria-label="Topic title"
+              placeholder="Enter topic name"
+              aria-label="Topic name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(', ')}
             />
             <Textarea
+              name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Enter topic description"
               aria-label="Topic description"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(', ')}
             />
             <Button type="submit">Submit</Button>
           </div>
